e31885a6d3abc561c2f6c0438451ccc624a00247
Izuchukwu chinedu Godson <chinedugodson31@gmail.com>
2025-09-01 15:59:42 +0100

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,19 +4,12 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
+export default defineConfig({
+  plugins: [react()],
   server: {
-    host: "::",
-    port: 8080,
-  },
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+    // Add this configuration
+    allowedHosts: [
+      'onehealth-shield.onrender.com'
     },
   },
 }));
